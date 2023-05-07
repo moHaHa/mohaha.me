595489eaee359d2ba6c78cc823ac62e955b8b7b2
moHaHa <diab.4869@gmail.com>
2023-05-07 13:15:13 +0300

    feat : meta data

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Mohammad Al Hallaq',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: '@mohaha, with a passion for creating beautiful and intuitive user experiences.' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
     },
+    { name: 'author', content: 'Mohammad Al Hallaq' },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:creator', content: '@mohaha300' },
   ],
   link: [
     {
