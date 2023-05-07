a2d260d6ed5ffbbe9b9ed610eda8c5ae323e20e5
moHaHa <diab.4869@gmail.com>
2023-05-07 13:08:29 +0300

    chore : clean up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,16 +2,6 @@
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
 </script>
 
 <template>
